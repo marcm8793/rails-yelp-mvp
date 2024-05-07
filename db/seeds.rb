afebd8ebd9d03e9c973fe9fc5c3b8bc83a0e626b
Marc Mansour <marcm9387@gmail.com>
2024-05-07 20:38:20 +0200

    exo yelp mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
@@ -7,3 +8,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Create 5 restaurants
+5.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    category: Restaurant::CATEGORIES.sample,
+    phone_number: Faker::PhoneNumber.phone_number
+  )
+end
+puts "5 restaurants created"
